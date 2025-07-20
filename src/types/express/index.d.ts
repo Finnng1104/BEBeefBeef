5d260572f612ce72ec4f62522bf47157e6f4fb9f
Finnng1104 <tiennt1104@gmail.com>
2025-07-21 01:27:28 +0700

    first commit

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -9,3 +9,11 @@ declare global {
 }
 
 export {};
+import 'express';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    file?: Express.Multer.File;
+    files?: Express.Multer.File[];
+  }
+}
---
+++ src/types/global.d.ts
@@ -0,0 +1,3 @@
+declare module 'streamifier';
+declare module 'nodemailer-express-handlebars';
+declare module '@paypal/checkout-server-sdk';
